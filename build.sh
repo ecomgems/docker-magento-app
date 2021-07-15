8df79e3f5e84b1829bb039970236dd6db2df997d
Igor Goltsov <riversy@gmail.com>
2021-07-15 19:58:53 +0300

    Add 7.1 image

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+(cd 7.1;docker build --no-cache -t ecomgems/magento-app:7.1 .)
+docker push ecomgems/magento-app:7.1
 (cd 7.3;docker build --no-cache -t ecomgems/magento-app:7.3 .)
 docker push ecomgems/magento-app:7.3
 (cd 7.4;docker build --no-cache -t ecomgems/magento-app:7.4 .)
