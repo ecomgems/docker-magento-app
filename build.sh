131245f56e1bb44df89fabf727e4b88ac5b8f28b
Igor Goltsov <riversy@gmail.com>
2021-11-07 15:27:32 +0300

    Add a production ready image for PHP 7.4

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-(cd 7.1;docker build --no-cache -t ecomgems/magento-app:7.1 .)
-docker push ecomgems/magento-app:7.1
-(cd 7.3;docker build --no-cache -t ecomgems/magento-app:7.3 .)
-docker push ecomgems/magento-app:7.3
-(cd 7.4;docker build --no-cache -t ecomgems/magento-app:7.4 .)
-docker push ecomgems/magento-app:7.4
-(cd 8.0;docker build --no-cache -t ecomgems/magento-app:8.0 .)
-docker push ecomgems/magento-app:8.0
+#(cd 7.1;docker build --no-cache -t ecomgems/magento-app:7.1 .)
+#docker push ecomgems/magento-app:7.1
+#(cd 7.3;docker build --no-cache -t ecomgems/magento-app:7.3 .)
+#docker push ecomgems/magento-app:7.3
+#(cd 7.4;docker build --no-cache -t ecomgems/magento-app:7.4 .)
+#docker push ecomgems/magento-app:7.4
+#(cd 8.0;docker build --no-cache -t ecomgems/magento-app:8.0 .)
+#docker push ecomgems/magento-app:8.0
+(cd prod-7.4;docker build --no-cache -t ecomgems/magento-app:prod-7.4 .)
+docker push ecomgems/magento-app:prod-7.4
