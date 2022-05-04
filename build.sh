87e3e41ba53cef023938c184225e5a51dfbfa31b
Igor Goltsov <riversy@gmail.com>
2022-05-04 10:41:03 +0300

    Add 8.1 image

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
-#(cd 7.1;docker build --no-cache -t ecomgems/magento-app:7.1 .)
-#docker push ecomgems/magento-app:7.1
-#(cd 7.3;docker build --no-cache -t ecomgems/magento-app:7.3 .)
-#docker push ecomgems/magento-app:7.3
-#(cd 7.4;docker build --no-cache -t ecomgems/magento-app:7.4 .)
-#docker push ecomgems/magento-app:7.4
-#(cd 8.0;docker build --no-cache -t ecomgems/magento-app:8.0 .)
-#docker push ecomgems/magento-app:8.0
+(cd 7.1;docker build --no-cache -t ecomgems/magento-app:7.1 .)
+docker push ecomgems/magento-app:7.1
+(cd 7.3;docker build --no-cache -t ecomgems/magento-app:7.3 .)
+docker push ecomgems/magento-app:7.3
+(cd 7.4;docker build --no-cache -t ecomgems/magento-app:7.4 .)
+docker push ecomgems/magento-app:7.4
+(cd 8.0;docker build --no-cache -t ecomgems/magento-app:8.0 .)
+docker push ecomgems/magento-app:8.0
+(cd 8.1;docker build --no-cache -t ecomgems/magento-app:8.1 .)
+docker push ecomgems/magento-app:8.1
 (cd prod-7.4;docker build --no-cache -t ecomgems/magento-app:prod-7.4 .)
 docker push ecomgems/magento-app:prod-7.4
