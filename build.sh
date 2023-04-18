793471c307bd94c3558493766cfda9ae803b4d52
Igor Goltsov <igor.goltsov@borngroup.com>
2023-04-18 09:26:28 +0300

    Add PHP 8.2

--- build.sh
+++ build.sh
@@ -10,5 +10,7 @@ docker push ecomgems/magento-app:7.4
 docker push ecomgems/magento-app:8.0
 (cd 8.1;docker build --no-cache -t ecomgems/magento-app:8.1 .)
 docker push ecomgems/magento-app:8.1
+(cd 8.2;docker build --no-cache -t ecomgems/magento-app:8.2 .)
+docker push ecomgems/magento-app:8.2
 (cd prod-7.4;docker build --no-cache -t ecomgems/magento-app:prod-7.4 .)
 docker push ecomgems/magento-app:prod-7.4
